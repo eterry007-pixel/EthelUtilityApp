2060711782324609bcc9e0158942e163539b2f7d
jd140047 <eterry007@gmail.com>
2026-02-19 19:51:49 +1000

    Test ViewModel, Retrofit

--- app/src/main/java/com/example/ethelutilityapp/MainActivity.kt
+++ app/src/main/java/com/example/ethelutilityapp/MainActivity.kt
@@ -20,15 +20,68 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ethelutilityapp.ui.theme.EthelUtilityAppTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+interface QuoteApi {
+    @GET("api/1.0/?method=getQuote&lang=en&format=text")
+    ////suspend fun: asynchronous code is called within a coroutine or another suspend function.
+    suspend fun getQuote(): String
+}
+
+//creates a single, static instance (a singleton)
+object RetrofitInstance {
+    val api: QuoteApi by lazy {//defers the initialization of the variable until it is first accessed.
+        Retrofit.Builder()
+            .baseUrl("http://api.forismatic.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+            .create(QuoteApi::class.java)
+    }
+}
+
+//ViewModel: store and manage UI-related data that survives configuration changes, such as screen rotations
+class QuoteViewModel : ViewModel() {
+    //MutableStateFlow: manage state changes in a reactive way.
+    private val _quote = MutableStateFlow("Click button to load quote")
+    val quote: StateFlow<String> = _quote //StateFlow: read-only version of MutableStateFlow.
+    fun loadQuote() {
+        //viewModelScope.launch is to start a Kotlin coroutine within a ViewModel
+        // that is automatically canceled when the ViewModel is destroyed (when its onCleared() method is called).
+        viewModelScope.launch {
+            try {
+                val result = RetrofitInstance.api.getQuote()
+                _quote.value = result
+            } catch (e: Exception) {
+                _quote.value = "Error: ${e.message}"
+            }
+        }
+    }
+}
+
+class CounterViewModel : ViewModel() {
+     private val _count = MutableStateFlow(0)
+     val count: StateFlow<Int> = _count
+     fun increment() {
+         _count.value++
+         }
+    }
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,6 +103,7 @@ fun EthelUtilityAppPreview() {
 @Composable
 fun EthelUtilityApp() {
     var selectedTab by remember { mutableStateOf("Utility") }
+    //var counter = 0
 
     Scaffold(
         bottomBar = {
@@ -79,7 +133,11 @@ fun EthelUtilityApp() {
 }
 @Composable
 fun UtilityScreen() {
-    var counter by remember { mutableIntStateOf(0) }
+    //var counter by remember { mutableIntStateOf(0) }
+    //val vm: CounterViewModel = viewModel()
+    //val counter by vm.count.collectAsState()
+    val vm: QuoteViewModel = viewModel()
+    val quote by vm.quote.collectAsState()
 
     Column(
         modifier = Modifier
@@ -88,13 +146,20 @@ fun UtilityScreen() {
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text("Utility Screen", style = MaterialTheme.typography.headlineMedium)
-        Text("Counter: $counter", style = MaterialTheme.typography.bodyLarge)
+        //Text("Counter: $counter", style = MaterialTheme.typography.bodyLarge)
 
-        Button(onClick = { counter++ }) {
-            Text("Increment")
-        }
+       // Button(onClick = { counter++ }) {
+            Button(onClick = { vm.loadQuote()}) {
+            //Button(onClick = { vm.increment() }) {
+               // Text("Increment")
+                Text("Get Quote")
+            }
+        Text(quote, style = MaterialTheme.typography.bodyLarge)
     }
 }
+        //}
+    //}
+//}
 @Composable
 fun SettingsScreen() {
     Column(
--- app/src/main/java/com/example/ethelutilityapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/ethelutilityapp/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.ethelutilityapp.ui.theme
+ package com.example.ethelutilityapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
